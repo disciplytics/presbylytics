6452404c4ba5e961f1ddd8e07d0004b9ac90d61c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 11:28:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from utils.utils import snowflake_connection
 from analysis.member_analysis import member_analysis
 from analysis.general_analysis import general_analysis
+from analysis.contributions_analysis import contributions_analysis
 
 # title
 st.title('PCA Statistics :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
@@ -61,5 +62,7 @@ with member_tab:
         member_analysis(filtered_df)
 with general_tab:
         general_analysis(filtered_df)
+with contrib_tab:
+        contributions_analysis(filtered_df)
 
 
