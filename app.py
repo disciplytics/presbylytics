3f4634cde482a99a78d2086b288becf689f79439
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 09:49:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,11 @@ filtered_df = df[
         (df['CHURCH'].isin(church_sel))
       ]
 
-st.scatter_chart(filtered_df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
+st.line_chart(
+  x = filtered_df['STAT_YEAR'].astype(str),
+  y = filtered_df[['COMM'], 'NON_COMM']]
+)
+#st.scatter_chart(filtered_df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 #st.scatter_chart(df, x ='Housing Characteristics,Average Household Size Of Occupied Housing Units By Tenure,Average household size,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 #st.scatter_chart(df, x ='Housing Characteristics,Median Selected Monthly Owner Costs As A Percentage Of Household Income In The Past 12 Months,Median selected monthly owner costs as a percentage of household income in the past 12 months,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 
