7426d6f1bba43a8e558ed8884f55bbb6dfd39d42
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 09:07:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import pandas as pd
 # title
 st.title('PCA Statistics :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
 
+# connect and load from snowflake
+conn = st.connection("snowflake")
+df = conn.query('select * from analytics_data', ttl=0)
+df.columns = [i.strip("'") for i in df.columns]
+
 # create filters
 
 
-conn = st.connection("snowflake")
 
-df = conn.query('select * from analytics_data', ttl=0)
-
-df.columns = [i.strip("'") for i in df.columns]
 
 st.scatter_chart(df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 #st.scatter_chart(df, x ='Housing Characteristics,Average Household Size Of Occupied Housing Units By Tenure,Average household size,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
