087d5f042567adf0d3b77b7f1b3942dd6f036abf
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-16 22:57:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ df = conn.query('select * from analytics_data', ttl=0)
 
 df.columns = [i.strip("'") for i in df.columns]
 
-st.scatter_chart(df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CHURCH_INCOME")
+st.scatter_chart(df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "PER_CAPITA_GIVING")
