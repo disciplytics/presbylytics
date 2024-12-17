4c23be1c9366448dab6871bd0613084b17e51d0e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 10:20:50 -0500

    Update member_analysis.py

--- analysis/member_analysis.py
+++ analysis/member_analysis.py
@@ -1,6 +1,7 @@
 def member_analysis():
-  st.subheader('Member Count Trends')
-  st.bar_chart(
+  from streamlit import subheader, bar_chart
+  subheader('Member Count Trends')
+  bar_chart(
           filtered_df.groupby(['Stat Year'])[['Comm', 'Non Comm']].sum(),
           y = ['Comm', 'Non Comm']
         )
