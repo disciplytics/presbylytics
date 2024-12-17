80e2d673f98b72b9b61125051bfee9be44b07657
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-16 22:59:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ df = conn.query('select * from analytics_data', ttl=0)
 df.columns = [i.strip("'") for i in df.columns]
 
 st.scatter_chart(df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
+st.scatter_chart(df, x ='Housing Characteristics,Average Household Size Of Occupied Housing Units By Tenure,Average household size,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
