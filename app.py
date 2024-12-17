42d7210eb724b8c1c8f3d7b050dc07de79496123
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 10:19:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from utils.utils import snowflake_connection
+from analysis.member_analysis import member_analysis
 
 # title
 st.title('PCA Statistics :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
@@ -56,11 +57,12 @@ tab_list = ['Member Data', 'General Data', 'Contributions Data', 'Benevol. Disbu
 member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
 # MEMBERS
 with member_tab:
-        st.subheader('Member Count Trends')
-        st.bar_chart(
-          filtered_df.groupby(['Stat Year'])[['Comm', 'Non Comm']].sum(),
-          y = ['Comm', 'Non Comm']
-        )
+        member_analysis()
+        #st.subheader('Member Count Trends')
+        #st.bar_chart(
+        #  filtered_df.groupby(['Stat Year'])[['Comm', 'Non Comm']].sum(),
+        #  y = ['Comm', 'Non Comm']
+        #)
 #st.scatter_chart(filtered_df, x ='Income,Gini Index Of Income Inequality,Gini Index,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 #st.scatter_chart(df, x ='Housing Characteristics,Average Household Size Of Occupied Housing Units By Tenure,Average household size,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
 #st.scatter_chart(df, x ='Housing Characteristics,Median Selected Monthly Owner Costs As A Percentage Of Household Income In The Past 12 Months,Median selected monthly owner costs as a percentage of household income in the past 12 months,Estimate', y = "TOTAL_CONTRIB", color = "STAT_YEAR")
