4d7a5e819d1849d29b533dd780905a66676297a4
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 12:36:44 -0500

    Update overview_analysis.py

--- analysis/overview_analysis.py
+++ analysis/overview_analysis.py
@@ -27,6 +27,14 @@ def overview_analysis(data):
   # calc giving per capita
   gpc = data.groupby(['Stat Year'])[['Total Contrib', 'Comm']].sum().reset_index()
   gpc = gpc.groupby(['Stat Year']).apply(lambda x: x['Total Contrib'] / x['Comm']).reset_index(name='Giving Per Capita')
+
+   # calc Benevolent per capita
+  bpc = data.groupby(['Stat Year'])[['Total Benevolent Disbursements', 'Comm']].sum().reset_index()
+  bpc = bpc.groupby(['Stat Year']).apply(lambda x: x['Total Benevolent Disbursements'] / x['Comm']).reset_index(name='Benevolent Disbursements Per Capita')
+
+  # calc Congregational per capita
+  epc = data.groupby(['Stat Year'])[['Current Expenses', 'Building Fund', 'Comm']].sum().reset_index()
+  epc = epc.groupby(['Stat Year']).apply(lambda x: (x['Current Expenses'] + x['Building Fund']) / x['Comm']).reset_index(name='Congregational Expenses Per Capita')
   
   st.write('Giving Per Capita')
   st.bar_chart(
@@ -39,12 +47,14 @@ def overview_analysis(data):
   with bpc_col:
     st.write('Benevolent Disbursements Per Capita: Total Benevolent Disbursements / Comm')
     st.bar_chart(
-            data.groupby(['Stat Year'])['Benevolent Disbursements Per Capita'].sum(),
-            y = 'Benevolent Disbursements Per Capita'
+            bpc,
+            y = 'Benevolent Disbursements Per Capita',
+            x = 'Stat Year'
           )
   with epc_col:
     st.write('Congregational Expenses Per Capita: (Current Expenses + Building Fund) / Comm')
     st.bar_chart(
-            data.groupby(['Stat Year'])['Congregational Expenses Per Capita'].sum(),
-            y = 'Congregational Expenses Per Capita'
+            epc,
+            y = 'Congregational Expenses Per Capita',
+            x = 'Stat Year'
           )
