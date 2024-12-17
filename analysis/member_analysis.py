ef68ff349b0345ab5e17687f8d650297aa7572a2
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 10:27:56 -0500

    Update member_analysis.py

--- analysis/member_analysis.py
+++ analysis/member_analysis.py
@@ -5,3 +5,9 @@ def member_analysis(data):
           data.groupby(['Stat Year'])[['Comm', 'Non Comm']].sum(),
           y = ['Comm', 'Non Comm']
         )
+
+  subheader('Additions & Losses')
+  bar_chart(
+          data.groupby(['Stat Year'])[['Prof Child',	'Prof Adult']].sum(),
+          y = ['Prof Child',	'Prof Adult']
+  )
