c53e7909d376a92e6239875b1c1964bf576cf8c5
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 09:10:18 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,7 @@
+# Util functions for the app
 
+def snowflake_connection(sql):
+  # connect and load from snowflake
+  from streamlit import connection
+  conn = connection("snowflake")
+  return conn.query(sql, ttl=0)
