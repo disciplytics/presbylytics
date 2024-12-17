2dcd6afc86bd6925fbeddaec843b20c72f5b3ea5
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 09:04:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # title
-st.title('PCA Statistics :church:', help = 'help str')
+st.title('PCA Statistics :church:', help = 'All data is from [The PCA](https://www.pcaac.org/stats/)')
 
 # create filters
 
