77f3e72468a36754c10a4d665ab65284c5f5280e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 10:52:24 -0500

    Update member_analysis.py

--- analysis/member_analysis.py
+++ analysis/member_analysis.py
@@ -1,22 +1,36 @@
 def member_analysis(data):
-  from streamlit import subheader, bar_chart
-  subheader('Member Count Trends')
-  bar_chart(
+  import streamlit as st
+  st.subheader('Member Count Trends')
+  st.bar_chart(
           data.groupby(['Stat Year'])[['Comm', 'Non Comm']].sum(),
           y = ['Comm', 'Non Comm']
         )
 
-  subheader('Additions & Losses')
+  st.subheader('Additions & Losses')
   additions = ['Prof Child', 'Prof Adult', 'Trans Letter', 'Reaffirmation', 'Restored']
   losses = ['Death', 'Trans', 'Removed From Roll', 'Discipline']
   
   for i in losses:
     data[i] = data[i] * -1 
     
-  bar_chart(
+  st.bar_chart(
           data.groupby(['Stat Year'])[additions + losses].sum(),
           y = additions + losses,
           stack = False
   )
+
+  st.subheader("Breakdown: Additions & Losses")
+  add_col, loss_col = st.columns(2)
+  years = data['Stat Year'].unique().to_list().sort()
+  default_statyear = years[0]
+  year_filter = st.selectbox('Select a Stat Year:', years, default_statyear)
+  break_df = data[data['Stat Year'] == year_filter]
+
+  st.write(f'Selected Year: {year_filter}')
+  with add_col:
+    st.bar_chart(
+      break_df,
+      x = additions)
+      
 
   
