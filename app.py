57dad4ec2fedc9cc13404a674177050165ba2d82
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-16 22:53:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ conn = st.connection("snowflake")
 
 df = conn.query('select * from analytics_data', ttl=0)
 
-st.scatter_chart(df, x =`'Income,Gini Index Of Income Inequality,Gini Index,Estimate'`, y = "TOTAL_CHURCH_INCOME")
+st.scatter_chart(df, x ="""'Income,Gini Index Of Income Inequality,Gini Index,Estimate'""", y = "TOTAL_CHURCH_INCOME")
