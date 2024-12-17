1ef4d247dded96901cff4dd0ad7a072171407bad
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 11:33:20 -0500

    Update contributions_analysis.py

--- analysis/contributions_analysis.py
+++ analysis/contributions_analysis.py
@@ -8,8 +8,16 @@ def contributions_analysis(data):
             y = 'Total Church Income'
           )
 
+  st.write('Giving Per Capita Trends')
+  st.bar_chart(
+            data.groupby(['Stat Year'])['Per Capita Giving'].sum(),
+            y = 'Per Capita Giving'
+          )
+  
   st.write('Contributions Breakdown')
   st.bar_chart(
             data.groupby(['Stat Year'])[['Tithes Offerings', 'Special Causes', 'Building Fund Offering', 'Other Contrib', 'Other Income']].sum(),
             y = ['Tithes Offerings', 'Special Causes', 'Building Fund Offering', 'Other Contrib', 'Other Income']
           )
+
+  
