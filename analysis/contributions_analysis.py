38229db130c4a1492d8e877ef9eb037f10d0b9e7
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-17 11:27:24 -0500

    Update contributions_analysis.py

--- analysis/contributions_analysis.py
+++ analysis/contributions_analysis.py
@@ -1,1 +1,15 @@
+def contributions_analysis(data):
+  import streamlit as st
+  st.subheader('Contribution Trends')
 
+  st.write('Total Church Income')
+  st.bar_chart(
+            data.groupby(['Stat Year'])['Total Church Income'].sum(),
+            y = 'Total Church Income'
+          )
+
+  st.write('Contributions Breakdown')
+  st.bar_chart(
+            data.groupby(['Stat Year'])[['Tithes Offerings', 'Special Causes', 'Building Fund Offering', 'Other Contrib', 'Other Income']].sum(),
+            y = 'Total Church Income'
+          )
