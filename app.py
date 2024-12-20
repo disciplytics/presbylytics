7e9f8f125094aa6803a4d8c3a064cc62b6ce0b68
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-20 11:17:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ st.title('PCA Statistics :church:', help = 'All data is from [The PCA](https://p
 
 with st.expander("Click to Learn More"):
         st.write("This app displays the Presbyterian Church in America statistics in an interactive frontend for enhanced analysis.")
+        st.write("Breakdown the analysis by state, city, or all churches")
 
 # connect and load from snowflake
 df = snowflake_connection('select * from metrics_data where stat_year <> 0')
@@ -22,53 +23,109 @@ df.columns = [i.strip("'").replace("_", " ").title() for i in df.columns]
 
 df['Stat Year'] = df['Stat Year'].astype(str)
 
-# create filters
-st.sidebar.subheader("Filtering Options For A Deep Dive")
-st.sidebar.write("Select A State")
+# create drilldown options
+options = ["All Churches", "Single Church"]
+selection = st.segmented_control(
+    "Drilldown Levels", options, selection_mode="single"
+)
 
-state_sel = st.sidebar.multiselect(
-    'Select Church State:', 
-    pd.Series(pd.unique(df['State'])).sort_values(),
-    pd.Series(pd.unique(df['State'])).sort_values()
-  )
-
-if len(state_sel) == 1:
-        filtered_df = df[df['State'].isin(state_sel)]
+if selection == "All Churches":
+        # create filters
+        st.sidebar.subheader("Filtering Options For A Deep Dive")
+        st.sidebar.write("Select A State")
         
-        city_sel = st.sidebar.multiselect(
-    'Select Church City:', 
-    pd.Series(pd.unique(filtered_df['City'])).sort_values(),
-    pd.Series(pd.unique(filtered_df['City'])).sort_values()
-  )
-
-        filtered_df = filtered_df[filtered_df['City'].isin(city_sel)]
-
+        state_sel = st.sidebar.multiselect(
+            'Select Church State:', 
+            pd.Series(pd.unique(df['State'])).sort_values(),
+            pd.Series(pd.unique(df['State'])).sort_values()
+          )
         
-        church_sel = st.sidebar.multiselect(
-    'Select Church Name:',
-    pd.Series(pd.unique(filtered_df['Church'])).sort_values(),
-    pd.Series(pd.unique(filtered_df['Church'])).sort_values()
-  )
+        if len(state_sel) == 1:
+                filtered_df = df[df['State'].isin(state_sel)]
+                
+                city_sel = st.sidebar.multiselect(
+            'Select Church City:', 
+            pd.Series(pd.unique(filtered_df['City'])).sort_values(),
+            pd.Series(pd.unique(filtered_df['City'])).sort_values()
+          )
+        
+                filtered_df = filtered_df[filtered_df['City'].isin(city_sel)]
+        
+                
+                church_sel = st.sidebar.multiselect(
+            'Select Church Name:',
+            pd.Series(pd.unique(filtered_df['Church'])).sort_values(),
+            pd.Series(pd.unique(filtered_df['Church'])).sort_values()
+          )
+        
+                filtered_df = filtered_df[filtered_df['Church'].isin(church_sel)]
+        else:
+                filtered_df = df.copy()
+        
+        tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
+        ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
+        
+        # analyses
+        with ch_tab:
+                churchhealth_analysis(filtered_df)
+        with member_tab:
+                member_analysis(filtered_df)
+        with general_tab:
+                general_analysis(filtered_df)
+        with contrib_tab:
+                contributions_analysis(filtered_df)
+        with benevol_tab:
+                benevol_disburs_analysis(filtered_df)
+        with cong_ops_tab:
+                congregational_ops_analysis(filtered_df)
 
-        filtered_df = filtered_df[filtered_df['Church'].isin(church_sel)]
-else:
-        filtered_df = df.copy()
+elif selection == "Single Church":
+        # create filters
+        st.sidebar.subheader("Filtering Options For A Deep Dive")
+        st.sidebar.write("Select A State")
+        
+        state_sel = st.sidebar.selectbox(
+            'Select Church State:', 
+            pd.Series(pd.unique(df['State'])).sort_values(),
+          )
+        
+        if len(state_sel) == 1:
+                filtered_df = df[df['State'].isin(state_sel)]
+                
+                city_sel = st.sidebar.selectbox(
+            'Select Church City:', 
+            pd.Series(pd.unique(filtered_df['City'])).sort_values(),
+          )
+        
+                filtered_df = filtered_df[filtered_df['City'].isin(city_sel)]
+        
+                if len(city_sel) == 1:
+                        church_sel = st.sidebar.selectbox(
+                    'Select Church Name:',
+                    pd.Series(pd.unique(filtered_df['Church'])).sort_values()
+                  )
+                
+                        filtered_df = filtered_df[filtered_df['Church'].isin(church_sel)]
+                
+        
+        tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
+        ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
+        
+        # analyses
+        with ch_tab:
+                churchhealth_analysis(filtered_df)
+        with member_tab:
+                member_analysis(filtered_df)
+        with general_tab:
+                general_analysis(filtered_df)
+        with contrib_tab:
+                contributions_analysis(filtered_df)
+        with benevol_tab:
+                benevol_disburs_analysis(filtered_df)
+        with cong_ops_tab:
+                congregational_ops_analysis(filtered_df)        
+        
 
-tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
-ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
 
-# analyses
-with ch_tab:
-        churchhealth_analysis(filtered_df)
-with member_tab:
-        member_analysis(filtered_df)
-with general_tab:
-        general_analysis(filtered_df)
-with contrib_tab:
-        contributions_analysis(filtered_df)
-with benevol_tab:
-        benevol_disburs_analysis(filtered_df)
-with cong_ops_tab:
-        congregational_ops_analysis(filtered_df)
         
 
