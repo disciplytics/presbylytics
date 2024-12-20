259a3145082207a75c668430f98127a31b9e98ee
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-20 14:12:56 -0500

    Create pca_metrics.py

---
+++ reports/pca_metrics.py
@@ -0,0 +1,18 @@
+def pca_metrics(df):
+  import streamlit as st
+  from millify import millify
+
+
+
+  st.subheader("Giving Metrics")
+
+  # calc giving per capita
+  gpc = df.groupby(['Stat Year'])[['Total Contrib', 'Comm']].sum().reset_index()
+  gpc = gpc.groupby(['Stat Year']).apply(lambda x: x['Total Contrib'] / x['Comm']).reset_index(name='Per Capita Giving')
+  gpc = gpc.sort_values(by = 'Stat Year', desc = True)
+
+  st.write(f'{gpc['Per Capita Giving'].loc[0]} and {gpc['Stat Year'].loc[0]})
+
+
+  
+  
