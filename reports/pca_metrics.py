c04bd4ab55ae9f48b5b0d0d8c3fb6a3a7d3eeeb9
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-20 14:17:30 -0500

    Update pca_metrics.py

--- reports/pca_metrics.py
+++ reports/pca_metrics.py
@@ -9,7 +9,7 @@ def pca_metrics(df):
   # calc giving per capita
   gpc = df.groupby(['Stat Year'])[['Total Contrib', 'Comm']].sum().reset_index()
   gpc = gpc.groupby(['Stat Year']).apply(lambda x: x['Total Contrib'] / x['Comm']).reset_index(name='Per Capita Giving')
-  gpc = gpc.sort_values(by = 'Stat Year', desc = True)
+  gpc = gpc.sort_values(by = 'Stat Year', ascending=False)
 
   st.write(f'{gpc["Per Capita Giving"].loc[0]} and {gpc["Stat Year"].loc[0]}')
 
