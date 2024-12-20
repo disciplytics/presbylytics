4aa68e8e9ac68e82a32727648dd94bdfca811830
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-20 14:53:20 -0500

    Update pca_metrics.py

--- reports/pca_metrics.py
+++ reports/pca_metrics.py
@@ -6,22 +6,30 @@ def pca_metrics(df):
 
   st.subheader("Giving Metrics")
 
-  # calc giving per capita
-  gpc = df.groupby(['Stat Year'])[['Total Contrib', 'Comm']].sum().reset_index()
-  gpc = gpc.groupby(['Stat Year']).apply(lambda x: x['Total Contrib'] / x['Comm']).reset_index(name='Per Capita Giving')
-  gpc = gpc.sort_values(by = 'Stat Year', ascending=True)
+  def get_pcg(df):
 
-  latest_year = gpc["Stat Year"].astype(int).max()
+    # calc giving per capita
+    gpc = df.groupby(['Stat Year'])[['Total Contrib', 'Comm']].sum().reset_index()
+    gpc = gpc.groupby(['Stat Year']).apply(lambda x: x['Total Contrib'] / x['Comm']).reset_index(name='Per Capita Giving')
+    gpc = gpc.sort_values(by = 'Stat Year', ascending=True)
+  
+    latest_year = gpc["Stat Year"].astype(int).max()
+  
+    pcg_lastest = millify(gpc[gpc["Stat Year"] == str(latest_year)]["Per Capita Giving"].values, precision=2)
+    pcg_2nd_lastest = millify(gpc[gpc["Stat Year"] == str(latest_year-1)]["Per Capita Giving"].values, precision=2)
 
-  pcg_lastest = millify(gpc[gpc["Stat Year"] == str(latest_year)]["Per Capita Giving"].values, precision=2)
-  pcg_2nd_lastest = millify(gpc[gpc["Stat Year"] == str(latest_year-1)]["Per Capita Giving"].values, precision=2)
-
-  pcg, pcb, pcc = st.columns(3)
-
-  with pcg:
     st.metric(label = "Per Capita Giving", 
               value = f'{latest_year}: {pcg_lastest}', 
               delta= f'{latest_year-1}: {pcg_2nd_lastest}', 
               delta_color="normal",
              label_visibility="visible", 
-              help = "PCG = Total Contrib / Comm ")
+              help = "PCG = Total Contrib / Comm")
+
+
+  
+
+  pcg, pcb, pcc = st.columns(3)
+
+  with pcg:
+    get_pcg(df)
+    
