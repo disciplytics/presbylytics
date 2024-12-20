f44d2a4f55d3c321b85162387eb1628f705d0c8c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-12-20 13:39:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,110 +30,118 @@ df.columns = [i.strip("'").replace("_", " ").title() for i in df.columns]
 
 df['Stat Year'] = df['Stat Year'].astype(str)
 
-# create drilldown options
-options = ["All Churches", "Single Church"]
-selection = st.segmented_control(
-    "Drilldown Levels", options, selection_mode="single", default ="All Churches"
+# create report type
+report_options = ["PCA Metrics" , "PCA Stats Deep Dives"]
+report_selection = st.segmented_control(
+    "Report Types", report_options, selection_mode="single", default ="PCA Metrics"
 )
 
-if selection == "All Churches":
-        # create filters
-        st.sidebar.subheader("Filtering Options For A Deep Dive")
-        st.sidebar.write("Select A State")
-        
-        state_sel = st.sidebar.multiselect(
-            'Select Church State:', 
-            pd.Series(pd.unique(df['State'])).sort_values(),
-            pd.Series(pd.unique(df['State'])).sort_values()
-          )
-        
-        if len(state_sel) == 1:
-                filtered_df = df[df['State'].isin(state_sel)]
-                
-                city_sel = st.sidebar.multiselect(
-            'Select Church City:', 
-            pd.Series(pd.unique(filtered_df['City'])).sort_values(),
-            pd.Series(pd.unique(filtered_df['City'])).sort_values()
-          )
-        
-                filtered_df = filtered_df[filtered_df['City'].isin(city_sel)]
-        
-                
-                church_sel = st.sidebar.multiselect(
-            'Select Church Name:',
-            pd.Series(pd.unique(filtered_df['Church'])).sort_values(),
-            pd.Series(pd.unique(filtered_df['Church'])).sort_values()
-          )
-        
-                filtered_df = filtered_df[filtered_df['Church'].isin(church_sel)]
-        else:
-                filtered_df = df.copy()
-        
-        tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
-        ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
-        
-        # analyses
-        with ch_tab:
-                churchhealth_analysis(filtered_df)
-        with member_tab:
-                member_analysis(filtered_df)
-        with general_tab:
-                general_analysis(filtered_df)
-        with contrib_tab:
-                contributions_analysis(filtered_df)
-        with benevol_tab:
-                benevol_disburs_analysis(filtered_df)
-        with cong_ops_tab:
-                congregational_ops_analysis(filtered_df)
 
-elif selection == "Single Church":
-        # create filters
-        st.sidebar.subheader("Filtering Options For A Single Church")
-        st.sidebar.write("Select A State")
-        
-        state_sel = st.sidebar.selectbox(
-            'Select Church State:', 
-            pd.Series(pd.unique(df['State'])).sort_values(),
-          )
-        
-        if state_sel:
-                filtered_df = df[df['State'] == state_sel]
-                
-                city_sel = st.sidebar.selectbox(
-            'Select Church City:', 
-            pd.Series(pd.unique(filtered_df['City'])).sort_values(),
-          )
-        
-                filtered_df = filtered_df[filtered_df['City'] == city_sel]
-        
-                if city_sel:
-                        church_sel = st.sidebar.selectbox(
-                    'Select Church Name:',
-                    pd.Series(pd.unique(filtered_df['Church'])).sort_values()
-                  )
-                
-                        filtered_df = filtered_df[filtered_df['Church'] == church_sel]
-                
-        
-                tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
-                ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
-                
-                # analyses
-                try:
-                        with ch_tab:
-                                churchhealth_analysis(filtered_df)
-                        with member_tab:
-                                member_analysis(filtered_df)
-                        with general_tab:
-                                general_analysis(filtered_df)
-                        with contrib_tab:
-                                contributions_analysis(filtered_df)
-                        with benevol_tab:
-                                benevol_disburs_analysis(filtered_df)
-                        with cong_ops_tab:
-                                congregational_ops_analysis(filtered_df)        
-                except:
-                        st.write('Church has bad data. Select another option.')
+if report_selection == "PCA Stats Deep Dives":
+    # create drilldown options
+    options = ["All Churches", "Single Church"]
+    selection = st.segmented_control(
+        "Drilldown Levels", options, selection_mode="single", default ="All Churches"
+    )
+    
+    if selection == "All Churches":
+            # create filters
+            st.sidebar.subheader("Filtering Options For A Deep Dive")
+            st.sidebar.write("Select A State")
+            
+            state_sel = st.sidebar.multiselect(
+                'Select Church State:', 
+                pd.Series(pd.unique(df['State'])).sort_values(),
+                pd.Series(pd.unique(df['State'])).sort_values()
+              )
+            
+            if len(state_sel) == 1:
+                    filtered_df = df[df['State'].isin(state_sel)]
+                    
+                    city_sel = st.sidebar.multiselect(
+                'Select Church City:', 
+                pd.Series(pd.unique(filtered_df['City'])).sort_values(),
+                pd.Series(pd.unique(filtered_df['City'])).sort_values()
+              )
+            
+                    filtered_df = filtered_df[filtered_df['City'].isin(city_sel)]
+            
+                    
+                    church_sel = st.sidebar.multiselect(
+                'Select Church Name:',
+                pd.Series(pd.unique(filtered_df['Church'])).sort_values(),
+                pd.Series(pd.unique(filtered_df['Church'])).sort_values()
+              )
+            
+                    filtered_df = filtered_df[filtered_df['Church'].isin(church_sel)]
+            else:
+                    filtered_df = df.copy()
+            
+            tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
+            ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
+            
+            # analyses
+            with ch_tab:
+                    churchhealth_analysis(filtered_df)
+            with member_tab:
+                    member_analysis(filtered_df)
+            with general_tab:
+                    general_analysis(filtered_df)
+            with contrib_tab:
+                    contributions_analysis(filtered_df)
+            with benevol_tab:
+                    benevol_disburs_analysis(filtered_df)
+            with cong_ops_tab:
+                    congregational_ops_analysis(filtered_df)
+    
+    elif selection == "Single Church":
+            # create filters
+            st.sidebar.subheader("Filtering Options For A Single Church")
+            st.sidebar.write("Select A State")
+            
+            state_sel = st.sidebar.selectbox(
+                'Select Church State:', 
+                pd.Series(pd.unique(df['State'])).sort_values(),
+              )
+            
+            if state_sel:
+                    filtered_df = df[df['State'] == state_sel]
+                    
+                    city_sel = st.sidebar.selectbox(
+                'Select Church City:', 
+                pd.Series(pd.unique(filtered_df['City'])).sort_values(),
+              )
+            
+                    filtered_df = filtered_df[filtered_df['City'] == city_sel]
+            
+                    if city_sel:
+                            church_sel = st.sidebar.selectbox(
+                        'Select Church Name:',
+                        pd.Series(pd.unique(filtered_df['Church'])).sort_values()
+                      )
+                    
+                            filtered_df = filtered_df[filtered_df['Church'] == church_sel]
+                    
+            
+                    tab_list = ['Church Health', 'Member Data', 'General Data', 'Contributions Data', 'Benevolent Disbursements Data', 'Conregation Ops']
+                    ch_tab, member_tab, general_tab, contrib_tab, benevol_tab, cong_ops_tab = st.tabs(tab_list)
+                    
+                    # analyses
+                    try:
+                            with ch_tab:
+                                    churchhealth_analysis(filtered_df)
+                            with member_tab:
+                                    member_analysis(filtered_df)
+                            with general_tab:
+                                    general_analysis(filtered_df)
+                            with contrib_tab:
+                                    contributions_analysis(filtered_df)
+                            with benevol_tab:
+                                    benevol_disburs_analysis(filtered_df)
+                            with cong_ops_tab:
+                                    congregational_ops_analysis(filtered_df)        
+                    except:
+                            st.write('Church has bad data. Select another option.')
         
 
 
