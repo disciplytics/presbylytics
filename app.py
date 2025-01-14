3d434f5df022b4b612eba87e75cc2b25399cffab
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-01-14 17:49:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,59 +34,50 @@ df.columns = [i.strip("'").replace("_", " ").title() for i in df.columns]
 df['Stat Year'] = df['Stat Year'].astype(str)
 
 # create report type
-report_options = ["PCA Overview" , "PCA Deep Dive"]
-report_selection = st.segmented_control(
-    "Analyses: ", report_options, selection_mode="single", default ="PCA Overview"
-)
-
-if report_selection == "PCA Overview":
-    # select a report
-    reportoption = st.selectbox(
+# select a report
+reportoption = st.selectbox(
         "Select a Report Type:",
         ("Membership", "General", "Contributions", "Benevol. Disbursments", "Congregational Ops."),
     )
 
-    # breakdowns
-    breakdown_options = ['All Churches', 'State', 'City']
-    breakoption = st.segmented_control(
+# breakdowns
+breakdown_options = ['All Churches', 'State', 'City']
+breakoption = st.segmented_control(
     "Breakdown By: ", breakdown_options, selection_mode="single", default ="All Churches"
 )
     
 
-    if breakoption == 'All Churches':
-        report_df = df.copy()
-    elif breakoption == 'State' or breakoption == 'City' :
-        state_sel = st.selectbox(
+if breakoption == 'All Churches':
+    report_df = df.copy()
+elif breakoption == 'State' or breakoption == 'City' :
+    state_sel = st.selectbox(
                     "Select a State:",
                     sorted(pd.unique(df['State'].dropna()).tolist())
         )
         
-        inter_df = df[df['State'] == state_sel]
+    inter_df = df[df['State'] == state_sel]
 
-        if breakoption == 'City':
-            city_sel = st.selectbox(
+    if breakoption == 'City':
+        city_sel = st.selectbox(
                     "Select a City:",
                     sorted(pd.unique(inter_df['City'].dropna()).tolist())
             )
-            report_df = inter_df[inter_df['City'] == city_sel]
+        report_df = inter_df[inter_df['City'] == city_sel]
 
-        elif breakoption == 'State':
-            report_df = inter_df.copy()
+    elif breakoption == 'State':
+        report_df = inter_df.copy()
             
-    # populate the reports
-    try:
-        if reportoption == "Membership":
-            member_analysis(report_df)
-        elif reportoption == "General":
-            general_analysis(report_df)
-        elif reportoption == "Contributions":
-            contributions_analysis(report_df)
-        elif reportoption == "Benevol. Disbursments":
-            benevol_disburs_analysis(report_df)
-        elif reportoption == "Congregational Ops.":
-            congregational_ops_analysis(report_df)
-    except:
-        st.write('Data is not good! Select Other Data.')
-    
-elif report_selection == "PCA Deep Dive":
-    deep_dive_report(df)
+# populate the reports
+try:
+    if reportoption == "Membership":
+        member_analysis(report_df)
+    elif reportoption == "General":
+        general_analysis(report_df)
+    elif reportoption == "Contributions":
+        contributions_analysis(report_df)
+    elif reportoption == "Benevol. Disbursments":
+        benevol_disburs_analysis(report_df)
+    elif reportoption == "Congregational Ops.":
+        congregational_ops_analysis(report_df)
+except:
+    st.write('Data is not good! Select Other Data.')
