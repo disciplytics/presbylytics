5b0c45c353d9d8d099c6d1bc6be1189d3aa6abae
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-01-14 17:59:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,12 +65,13 @@ elif breakoption == 'State' or breakoption == 'City' or breakoption == 'Church':
         inter_2_df = inter_df[inter_df['City'] == city_sel]
         
         if breakoption == 'Church':
-            church_sel = st.selectbox(
-                    "Select a Church:",
-                    sorted(pd.unique(inter_df['Church'].dropna()).tolist())
-            )
-            
-            report_df = inter_2_df[inter_2_df['Church'] == church_sel]
+            if city_sel:
+                church_sel = st.selectbox(
+                        "Select a Church:",
+                        sorted(pd.unique(inter_df['Church'].dropna()).tolist())
+                )
+                
+                report_df = inter_2_df[inter_2_df['Church'] == church_sel]
         
         elif breakoption == 'City':
             report_df = inter_2_df.copy()
